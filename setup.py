b605ac12ca395bc79276b71ad2c0e6be75f07373
Abraham Owodunni <Abrahamowodunni@gmail.com>
2024-04-20 18:56:14 -0700

    setup.py created'

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+E_DOT = '-e .'
+
+# creating a function that helps us get the required libraries 
+def get_requirments(file_path:str)->List[str]:
+    """
+    Reads the specified requirements file and returns a list of requirements.
+    Args:
+        file_path (str): The path to the requirements file.
+    Returns:
+        List[str]: A list of requirements extracted from the file.
+    """
+    requirments = []
+    with open(file_path) as f:
+        requirments = f.readlines()
+        requirments = [req.replace("\n","") for req in requirments]
+
+        if E_DOT in requirments:
+            requirments.remove(E_DOT)
+
+    return requirments
+
+
+setup(
+    name = 'Industry MLOps',
+    version= '0.0.1',
+    author= 'Abraham Owodunni',
+    author_email= 'abrahamowodunni@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirments('requirements.txt')
+)
